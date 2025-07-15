fba6c7ba7e1f1ecdf523cd852a43706718182162
Musa <musa.ismailov.2003@gmail.com>
2025-07-15 05:22:14 +0300

    добавляет JS  и пишет пару функций

---
+++ js/functions.js
@@ -0,0 +1,28 @@
+const wordLength = (word, number) =>{
+  return word.length <= number;
+};
+
+const palindrome = (word) => {
+  word = word.replaceAll(" ", "").toUpperCase();
+  let reversedWord = "";
+  for(let i = word.length - 1; i >= 0 ; i--){
+    reversedWord += word[i];
+  }
+  return word === reversedWord;
+};
+
+// const palindrome = (word) => {
+//   const newWord = word.replaceAll(" ", "").toUpperCase();
+//   return newWord === newWord.split("").reverse().join("");
+// };     еще один способ
+
+const devise = (oneWord) => {
+  let newOneWord = "";
+  for (let i = 0 ; i <= oneWord.length - 1 ; i++){
+    const num = parseInt(oneWord[i], 10);
+    if (!Number.isNaN(num)){
+      newOneWord += oneWord[i];
+    }
+  }
+  return newOneWord || NaN;
+}
