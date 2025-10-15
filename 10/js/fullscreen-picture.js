6f101d407a17953f4c365c1687c07ea374483067
Keks <keks@htmlacademy.ru>
2025-10-16 00:28:12 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/fullscreen-picture.js
@@ -0,0 +1,102 @@
+import {isEscapeKey} from './utils.js';
+
+let COMMENTS_SHOWN = 5;
+
+const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+const bigPicture = document.querySelector('.big-picture');
+const bigPictureImg = document.querySelector('.big-picture__img img');
+const bigPictureLikes = document.querySelector('.likes-count');
+const bigPictureShownCount = document.querySelector('.social__comment-shown-count');
+const bigPictureTotalCount = document.querySelector('.social__comment-total-count');
+const bigPictureDescription = document.querySelector('.social__caption');
+const bigPictureClosedElement = bigPicture.querySelector('.cancel');
+const socialComments = document.querySelector('.social__comments');
+const bigPictureCommentsLoader = bigPicture.querySelector('.comments-loader');
+
+let currentPhotoData = null;
+
+function onDocumentKeydown (evt){
+  if (isEscapeKey(evt.key)) {
+    evt.preventDefault();
+    closePhotoModal();
+  }
+}
+
+function closePhotoModal () {
+  bigPicture.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+
+  COMMENTS_SHOWN = 5;
+}
+
+const openPhotoModal = () => {
+  bigPicture.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const createCommentElement = (comment) => {
+  const commentElement = commentTemplate.cloneNode(true);
+
+  commentElement.querySelector('.social__picture').src = comment.avatar;
+  commentElement.querySelector('.social__picture').alt = comment.name;
+  commentElement.querySelector('.social__text').textContent = comment.message;
+
+  return commentElement;
+};
+
+const createComments = (comments, limit) => {
+  const limitedComments = comments.slice(0, limit);
+  const fragment = document.createDocumentFragment();
+
+  limitedComments.forEach((comment) => {
+    const commentElement = createCommentElement(comment);
+
+    fragment.appendChild(commentElement);
+  });
+
+  socialComments.append(fragment);
+};
+
+const updateComments = (photoData) => {
+  if (!photoData) {
+    return;
+  }
+
+  socialComments.innerHTML = '';
+
+  const totalComments = photoData.comments.length;
+  const commentsToShow = Math.min(COMMENTS_SHOWN, totalComments);
+
+  createComments(photoData.comments, commentsToShow);
+  bigPictureShownCount.textContent = commentsToShow;
+  bigPictureTotalCount.textContent = totalComments;
+
+  bigPictureCommentsLoader.classList.toggle('hidden', commentsToShow >= totalComments);
+};
+
+export const openBigPicture = (photoData) => {
+  socialComments.innerHTML = '';
+
+  currentPhotoData = photoData;
+  bigPictureDescription.textContent = photoData.description;
+  bigPictureLikes.textContent = photoData.likes;
+  bigPictureImg.src = photoData.url;
+
+  updateComments(currentPhotoData);
+  openPhotoModal();
+};
+
+
+bigPictureClosedElement.addEventListener('click', () => {
+  closePhotoModal();
+});
+
+bigPictureCommentsLoader.addEventListener('click',(evt) => {
+  evt.preventDefault();
+
+  COMMENTS_SHOWN += 5;
+
+  updateComments(currentPhotoData);
+});
