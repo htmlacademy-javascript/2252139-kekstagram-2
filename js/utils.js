a9f501e81f54896cf182f717c70e38a7058c8b97
Musa-Ismailov <164714080+Musa-Ismailov@users.noreply.github.com>
2025-12-15 15:43:24 +0300

    создает правки 1

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,4 @@
-export const getRandomInteger = (a, b) => {
+const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
