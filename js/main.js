b9e20ba827af98693b8a9c5c7b128ecec3934bb0
Musa-Ismailov <164714080+Musa-Ismailov@users.noreply.github.com>
2025-12-24 16:35:09 +0300

    создает правки для защиты 2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-import './gallery.js';
 import './fullscreen-picture.js';
 import './upload-picture.js';
 import './form.js';
-import './api.js';
 
 import { showDataError } from './dialogs.js';
 import { renderGallery, setGalleryData } from './gallery.js';
